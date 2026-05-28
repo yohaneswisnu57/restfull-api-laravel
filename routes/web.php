d3d651def58370707bd70bad8c69688ebc43716c
Yohanes Wisnu Perdana <wisnuyohanes33@gmail.com>
2026-05-28 14:54:17 +0700

    feat: add interactive OpenAPI documentation page and route handler

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Interactive API Documentation
+Route::get('/docs', function () {
+    return response()->file(public_path('docs.html'));
+});
